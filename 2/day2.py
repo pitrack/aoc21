0e47db1a1723a8e621b27dafd56d9fa57cc94474
pitrack <paxia@jhu.edu>
2021-12-02 02:12:15 -0500

    day 2

---
+++ 2/day2.py
@@ -0,0 +1,25 @@
+import torch
+torch.set_default_tensor_type('torch.cuda.FloatTensor')
+torch.set_printoptions(sci_mode=False)
+# CPU: 0.68s, GPU: 2.2s
+
+# Read in lines
+word_map = {
+    "forward": torch.Tensor([1, 0]),
+    "down": torch.Tensor([0, 1]),
+    "up": torch.Tensor([0, -1])
+}
+
+in_file = open("input", 'r')
+movements = torch.stack([int(line.split()[1]) * word_map[line.split()[0]]
+                          for line in in_file.readlines()])
+
+# Part 1
+final_loc = torch.sum(movements, dim=0)
+print(torch.prod(final_loc))
+
+# Part 2
+h_dir, a_dir = torch.unbind(movements, dim=1)
+total_v_dir = h_dir * (torch.tril(torch.ones(h_dir.shape[0], h_dir.shape[0])).matmul(a_dir))
+new_movements = torch.stack((h_dir, total_v_dir), dim=1)
+print(torch.prod(torch.sum(new_movements, dim=0).int()))
